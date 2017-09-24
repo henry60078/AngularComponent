96fcd86536a6c0750734b1d517dfc358db84281f
Henry Teng <harry60078@gmail.com>
2017-09-24 11:24:59 +0800

    commit other files

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  title = 'The Will Will Web';
+  link = 'http://blog.miniasp.com/';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
